da6d124ce1f3e98eb91b94f2b24133a053b6d59f
Sofbeta <150270220+Sofbeta@users.noreply.github.com>
2024-09-03 21:14:01 -0600

    sofia.c

--- sofia.c
+++ sofia.c
@@ -5,7 +5,8 @@ void invertir_matriz_2x2(float matriz[2][2], float inversa[2][2])
     float determinante = matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0];
 
     // Verificamos si la matriz es invertible
-    if (determinante == 0.0) {
+    if (determinante == 0.0) 
+    {
         printf("La matriz no es invertible.\n");
         return;
     }
@@ -21,8 +22,10 @@ int main() {
     float matriz[2][2], inversa[2][2];
 
     printf("Ingrese los elementos de la matriz 2x2:\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 2; i++) 
+    {
+        for (int j = 0; j < 2; j++)
+        {
             scanf("%f", &matriz[i][j]);
         }
     }
@@ -30,8 +33,10 @@ int main() {
     invertir_matriz_2x2(matriz, inversa);
 
     printf("La matriz inversa es:\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 2; i++) 
+    {
+        for (int j = 0; j < 2; j++) 
+        {
             printf("%.2f ", inversa[i][j]); 
         }
         printf("\n");
