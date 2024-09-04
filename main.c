7587880fdf356737ead65a3081e339d4283732cf
Umbral1013 <Sixela121@hotmail.com>
2024-09-03 19:18:55 -0600

    Commit inicial.

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * Nombre del programa:
+ * Fecha: mar 03 sep 2024 17:11:23
+ * Descripcion del programa:
+ *
+ * Nombres de los programadores:
+ *   - Alan Octavio Ramirez Hernandez
+ *   - Raquel Estefania Quevedo Rodriguez
+ *   - Eduardo Alejandro Enriquez Velazquez
+ *   - Lilia Sofia Betancourt Haro
+ *   - Alexis Gabriel Centeno Uribe
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hola Norma Elva.");
+	return 0;
+}
+
